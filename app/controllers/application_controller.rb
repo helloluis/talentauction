92cb5fefc393a2bd75c2e6dd81b4b8ec196751c8
Buddy Magsipoc <keikun17@gmail.com>
2014-03-06 15:45:56 +0800

    -pry。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    binding.pry
     if resource.is_a? Professional
       edit_professional_registration_path
     elsif resource.is_a? Recruiter
