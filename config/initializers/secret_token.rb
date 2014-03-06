236b46782e970c985de587c61feb1151e91b8e69
Luis Buenaventura <luis@syndeolabs.com>
2014-03-06 08:07:36 +0800

    added secret_token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talentauction::Application.config.secret_key_base = '6b8b34481038e6222e5f23d1502d4a34dc7dad2740d5719414690e1e4325dfcbe33ea461c27bca9f540d6bce1167b1531ebdbe2007aa88937acf3f3cb699d740'
