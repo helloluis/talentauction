dbfd0c929a1c6466878cf6e960c51be31397e891
Buddy Magsipoc <keikun17@gmail.com>
2014-03-07 09:01:36 +0800

    Rebuild schema, sorry about that。

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,5 +1,5 @@
 class Professional < User
-  has_one :profile, class_name: 'ProfessionalProfile', autosave: true, foreign_key: :user_id
+  has_one :profile, class_name: 'ProfessionalProfile', autosave: true
   before_create :build_profile
 
   accepts_nested_attributes_for :profile
