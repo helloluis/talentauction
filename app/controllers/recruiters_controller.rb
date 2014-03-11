097a4b540b233fc31bc2bd4d7436d9539d3808b1
Buddy Magsipoc <keikun17@gmail.com>
2014-03-11 17:56:25 +0800

    Recruiter Requirement stub page。

--- app/controllers/recruiters_controller.rb
+++ app/controllers/recruiters_controller.rb
@@ -1,5 +1,34 @@
 class RecruitersController < ApplicationController
+  before_action :set_recruiter
+
   def edit_profile
+
+  end
+
+  def update
+    if @recruiter.update_attributes(recruiter_params)
+      redirect_to recruiter_edit_requirement_path(@recruiter)
+    else
+    end
+  end
+
+  def edit_requirement
+    
+  end
+
+  private
+
+  def set_recruiter
     @recruiter = current_recruiter
+  end
+
+  def recruiter_params
+    params.require(:recruiter).permit( profile_attributes: [:company_name,
+                                                           :company_size,
+                                                           :industry,
+                                                           :country,
+                                                           :city,
+                                                           :summary,
+                                                           :additional_info ] )
   end
 end
